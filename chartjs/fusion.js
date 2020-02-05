976162b20d09416d2967b188b4696d236c8a9e97
fujii0716 <s_fujii@matisse.co.jp>
2020-02-05 17:01:40 +0900

    Initial commit

---
+++ chartjs/fusion.js
@@ -0,0 +1,22 @@
+// jQuery plugin to prevent double submission of forms
+jQuery.fn.preventDoubleSubmission = function() {
+    $(this).on('submit',function(e){
+        var $form = $(this);
+
+        if ($form.data('submitted') === true) {
+            // Previously submitted - don't submit again
+            e.preventDefault();
+        } else {
+            // Mark it so that the next submit can be ignored
+            $form.data('submitted', true);
+            setTimeout(function(){ $form.data('submitted', false); }, 150000);
+        }
+    });
+
+    // Keep chainability
+    return this;
+};
+
+$( document ).ready(function() {
+    $('form').preventDoubleSubmission();
+});
